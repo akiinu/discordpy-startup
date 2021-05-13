0e69054fa8f57e89fccb2d4b42826337de0921d3
akiinu <80990672+akiinu@users.noreply.github.com>
2021-05-13 20:42:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -11,11 +12,6 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
 
 
 
@@ -23,12 +19,9 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("pong")
     
-async def on_message(message):
-    if message.content.startswith("!tnp"): #ここの!diceは好きなのにしていいぞ
-        if client.user != message.author:
-            num_random = random.randrange(1,20)
-            m = str(num_random)
-            await client.send_message(message.channel, m)
+if message.content == "dice":
+    dice = random.randint(1, 20) #出る目を指定
+    await message.send_message(message.channel, str(dice))
 
     
 
